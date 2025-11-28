dda45fa0db336451be8762eacf53a56fb2f85f91
smchoi <seonmun.choi@gmail.com>
2025-11-28 10:48:56 +0900

    feat: 콘솔 초기화 기능 구현

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 초기화
+            Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Title = "My Sokoban";
+            Console.CursorVisible = false;
+            Console.Clear();
         }
     }
 }
