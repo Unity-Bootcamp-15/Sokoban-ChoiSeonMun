e4a3f5ca98d27150a7f23a6c97b602c060eed94e
smchoi <seonmun.choi@gmail.com>
2026-01-08 08:59:03 +0900

    refact: Position 값 개체 생성

---
+++ Sokoban/Sokoban/Position.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Sokoban
+{
+    // Position 타입끼리의 비교
+    internal struct Position : IEquatable<Position>
+    {
+        private int _x;
+        private int _y;
+
+        public int X => _x;
+        public int Y => _y;
+
+        private Position(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public static Position At(int x, int y)
+        {
+            return new Position(x, y);
+        }
+
+        public bool Equals(Position other)
+        {
+            return (X == other.X && Y == other.Y);
+        }
+
+
+        // 같음 연산자 ==
+        public static bool operator ==(Position left, Position right) => left.Equals(right);
+        public static bool operator !=(Position left, Position right) => !(left == right);
+    }
+}
--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -4,6 +4,22 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace Sokoban
 {
+
+    // 개체 지향 프로그래밍: 개체
+    // ㄴ 상태와 행위를 가진 것
+
+    // 어떤 데이터끼리 연관이 있는가?
+    // 그 데이터를 조작하는 함수는 무엇인가?
+
+    // 게임오브젝트
+    // ㄴ플레이어
+    // ㄴ벽
+    // ㄴ박스
+    // ㄴ골
+
+    // 게임
+
+    // X, Y를 묶은 개체 => Position
     internal class Program
     {
         enum Direction
@@ -27,32 +43,42 @@ namespace Sokoban
             Console.Clear();
 
             // 게임 데이터 초기화
-            int mapSizeMinX = 0;
-            int mapSizeMinY = 0;
-            int mapSizeMaxX = 10;
-            int mapSizeMaxY = 10;
+
+            Position mapMinSize = Position.At(0, 0);
+            Position mapMaxSize = Position.At(10, 10);
             bool isGameOver = false;
 
             // 플레이어 데이터
-            int playerX = 5;
-            int playerY = 10;
+            Position playerPosition = Position.At(5, 10);
             Direction playerDirection = Direction.None;
 
             // 벽 데이터
-            int[] wallX = { 3, 4, 5, 6, 7 };
-            int[] wallY = { 3, 3, 3, 3, 3 };
-            int wallCount = wallX.Length;
+            Position[] wallPositions = new Position[5]
+            {
+                Position.At(3, 3),
+                Position.At(4, 3),
+                Position.At(5, 3),
+                Position.At(6, 3),
+                Position.At(7, 3),
+            };
+            int wallCount = wallPositions.Length;
 
             // 박스 데이터
-            int[] boxX = { 6, 8 };
-            int[] boxY = { 6, 7 };
-            int boxCount = boxX.Length;
+            Position[] boxPositions = new Position[2]
+            {
+                Position.At(6, 6),
+                Position.At(8, 7)
+            };
+            int boxCount = boxPositions.Length;
 
             // 골 데이터
-            int[] goalX = { 4, 7 };
-            int[] goalY = { 5, 10 };
+            Position[] goalPositions = new Position[2]
+            {
+                Position.At(4, 5),
+                Position.At(7, 10)
+            };
             bool[] isBoxOnGoal = { false, false };
-            int goalCount = goalX.Length;
+            int goalCount = goalPositions.Length;
 
             // ------------ 게임 루프 -----------
             while (isGameOver == false)
@@ -69,25 +95,25 @@ namespace Sokoban
             {
                 Console.Clear();
 
-                RenderObjects(boxX, boxY, boxCount, _ => "@");
-                RenderObjects(goalX, goalY, goalCount, idx => isBoxOnGoal[idx] ? "*" : "O");
-                RenderObject(playerX, playerY, "P");
-                RenderObjects(wallX, wallY, wallCount, _ => "#");
+                RenderObjects(boxPositions, boxCount, _ => "@");
+                RenderObjects(goalPositions, goalCount, idx => isBoxOnGoal[idx] ? "*" : "O");
+                RenderObject(playerPosition, "P");
+                RenderObjects(wallPositions, wallCount, _ => "#");
 
                 // ---------------------------------------------------
 
-                void RenderObject(int x, int y, string symbol)
+                void RenderObject(Position pos, string symbol)
                 {
-                    Console.SetCursorPosition(x, y); ;
+                    Console.SetCursorPosition(pos.X, pos.Y);
                     Console.Write(symbol);
                 }
 
                 // NOTE: 조건부 심볼이 있어 Func를 받는다.
-                void RenderObjects(int[] x, int[] y, int count, Func<int, string> symbolSelector)
+                void RenderObjects(Position[] positions, int count, Func<int, string> symbolSelector)
                 {
                     for (int i = 0; i < count; ++i)
                     {
-                        RenderObject(x[i], y[i], symbolSelector(i));
+                        RenderObject(positions[i], symbolSelector(i));
                     }
                 }
             }
@@ -114,17 +140,16 @@ namespace Sokoban
                     _ => Direction.None
                 };
 
-                (int newX, int newY) GetNewPositionByDirection(Direction direction, int oldX, int oldY) => direction switch
+                // 필드를 조작하는 함수는 Position을 조작하는 것도 상위 개체에서 만드는 게 옳더.
+                Position GetNewPositionByDirection(Direction direction, Position old) => direction switch
                 {
-                    Direction.Left => (Math.Max(mapSizeMinX, oldX - 1), oldY),
-                    Direction.Right => (Math.Min(mapSizeMaxX, oldX + 1), oldY),
-                    Direction.Up => (oldX, Math.Max(mapSizeMinY, oldY - 1)),
-                    Direction.Down => (oldX, Math.Min(mapSizeMaxY, oldY + 1))
+                    Direction.Left => Position.At(Math.Max(mapMinSize.X, old.X - 1), old.Y),
+                    Direction.Right => Position.At(Math.Min(mapMaxSize.X, old.X + 1), old.Y),
+                    Direction.Up => Position.At(old.X, Math.Max(mapMinSize.Y, old.Y - 1)),
+                    Direction.Down => Position.At(old.X, Math.Min(mapMaxSize.Y, old.Y + 1))
                 };
 
-                bool IsSamePosition(int x1, int y1, int x2, int y2) => (x1 == x2 && y1 == y2);
-
-                int GetCollidedIndex(int x, int y, int[] targetX, int[] targetY, int targetCount, int excludedIndex = -1)
+                int GetCollidedIndex(Position pos, Position[] targetPositions, int targetCount, int excludedIndex = -1)
                 {
                     int found = -1;
                     for (int idx = 0; idx < targetCount; ++idx)
@@ -134,7 +159,7 @@ namespace Sokoban
                             continue;
                         }
 
-                        if (IsSamePosition(x, y, targetX[idx], targetY[idx]))
+                        if (pos == targetPositions[idx])
                         {
                             found = idx;
                             break;
@@ -144,18 +169,17 @@ namespace Sokoban
                     return found;
                 }
 
-                bool IsCollided(int x, int y, int[] targetX, int[] targetY, int targetCount, int excludedIndex = -1)
+                bool IsCollided(Position pos, Position[] targetPositions, int targetCount, int excludedIndex = -1)
                 {
-                    return -1 != GetCollidedIndex(x, y, targetX, targetY, targetCount, excludedIndex);
+                    return -1 != GetCollidedIndex(pos, targetPositions, targetCount, excludedIndex);
                 }
 
                 void UpdateGoalState()
                 {
                     for (int goalIdx = 0; goalIdx < goalCount; ++goalIdx)
                     {
-                        int currentGoalX = goalX[goalIdx];
-                        int currentGoalY = goalY[goalIdx];
-                        isBoxOnGoal[goalIdx] = IsCollided(currentGoalX, currentGoalY, boxX, boxY, boxCount);
+                        Position currentGoalPosition = goalPositions[goalIdx];
+                        isBoxOnGoal[goalIdx] = IsCollided(currentGoalPosition, boxPositions, boxCount);
                     }
                 }
 
@@ -171,10 +195,10 @@ namespace Sokoban
                         return false;
                     }
 
-                    var (newPlayerX, newPlayerY) = GetNewPositionByDirection(playerDirection, playerX, playerY);
+                    Position newPlayerPosition = GetNewPositionByDirection(playerDirection, playerPosition);
 
                     // 플레이어와 벽의 충돌 처리
-                    if (IsCollided(newPlayerX, newPlayerY, wallX, wallY, wallCount))
+                    if (IsCollided(newPlayerPosition, wallPositions, wallCount))
                     {
                         return false;
                     }
@@ -182,34 +206,31 @@ namespace Sokoban
                     // 플레이어와 박스의 충돌 처리
                     // 1. 플레이어가 어떤 박스와 충돌했는지 찾는다.
                     const int NoCollidedBox = -1;
-                    int collidedBoxIndex = GetCollidedIndex(newPlayerX, newPlayerY, boxX, boxY, boxCount);
+                    int collidedBoxIndex = GetCollidedIndex(newPlayerPosition, boxPositions, boxCount);
                     if (collidedBoxIndex != NoCollidedBox)
                     {
                         // 2-1. 박스의 새로운 좌표를 구한다.
-                        int currentBoxX = boxX[collidedBoxIndex];
-                        int currentBoxY = boxY[collidedBoxIndex];
-                        var (newBoxX, newBoxY) = GetNewPositionByDirection(playerDirection, currentBoxX, currentBoxY);
+                        Position currentBoxPosition = boxPositions[collidedBoxIndex];
+                        Position newBoxPosition = GetNewPositionByDirection(playerDirection, currentBoxPosition);
 
                         // 2-2. 벽과의 충돌 처리
-                        if (IsCollided(newBoxX, newBoxY, wallX, wallY, wallCount))
+                        if (IsCollided(newBoxPosition, wallPositions, wallCount))
                         {
                             return false;
                         }
 
                         // 2-3. 박스끼리의 충돌 처리
-                        if (IsCollided(newBoxX, newBoxY, boxX, boxY, boxCount, collidedBoxIndex))
+                        if (IsCollided(newBoxPosition, boxPositions, boxCount, collidedBoxIndex))
                         {
                             return false;
                         }
 
                         // 2-4. 박스의 좌표를 갱신한다.
-                        boxX[collidedBoxIndex] = newBoxX;
-                        boxY[collidedBoxIndex] = newBoxY;
+                        boxPositions[collidedBoxIndex] = newBoxPosition;
                     }
 
                     // 플레이어의 좌표를 갱신한다.
-                    playerX = newPlayerX;
-                    playerY = newPlayerY;
+                    playerPosition = newPlayerPosition;
 
                     return true;
                 }
