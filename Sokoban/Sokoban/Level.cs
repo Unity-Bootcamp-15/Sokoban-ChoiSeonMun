25e4af78b9d8e3f24395566b36bd846d18f63381
smchoi <seonmun.choi@gmail.com>
2026-01-15 11:51:03 +0900

    refactor: Level에 빌더 패턴 적용

--- Sokoban/Sokoban/Level.cs
+++ Sokoban/Sokoban/Level.cs
@@ -2,65 +2,92 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sokoban
 {
+    // DTO(Data Transfer Object)
+    public class LevelData
+    {
+        public Position Player { get; set; }
+        public List<Position> Wall { get; set; }
+        public List<Position> Box { get; set; }
+        public List<Position> Goal { get; set; }
+        public Size MapSize { get; set; }
+    }
+
     public class Level
     {
-        public GameObject Player { get; set; }
-        public List<GameObject> Walls { get; set; }
-        public List<GameObject> Boxes { get; set; }
-        public List<GameObject> Goals { get; set; }
+        private readonly GameObject _player;
+        private readonly List<GameObject> _walls;
+        private readonly List<GameObject> _boxes;
+        private readonly List<GameObject> _goals;
+        private readonly Map _map;
 
-        public Map Map { get; set; }
+        public IEnumerable<GameObject> AllObject => _walls.Concat(_boxes).Concat(_goals).Append(_player);
 
-        public IEnumerable<GameObject> Obstacles => Boxes.Concat(Walls);
-        public IEnumerable<GameObject> AllObject => Obstacles.Concat(Goals).Append(Player);
+        private Level(LevelData data)
+        {
+            _player = GameObjectFactory.Create(GameObjectType.Player, data.Player);
+            _walls = data.Wall.ConvertAll(wallPos => GameObjectFactory.Create(GameObjectType.Wall, wallPos));
+            _boxes = data.Box.ConvertAll(boxPos => GameObjectFactory.Create(GameObjectType.Box, boxPos));
+            _goals = data.Goal.ConvertAll(goalPos => GameObjectFactory.Create(GameObjectType.Goal, goalPos));
 
-        public bool TryMovePlayer(Direction direction) => Map.TryMove(Player, direction);
+            IEnumerable<GameObject> obstacles = _boxes.Concat(_walls);
+            _map = new(data.MapSize, obstacles);
+        }
+
+        public bool TryMovePlayer(Direction direction) => _map.TryMove(_player, direction);
 
         public void UpdateGoalState()
         {
-            foreach (Goal goal in Goals.Cast<Goal>())
+            foreach (Goal goal in _goals.Cast<Goal>())
             {
-                goal.HasBox = Boxes.ExistsAt(goal.Position);
+                goal.HasBox = _boxes.ExistsAt(goal.Position);
             }
         }
 
         public bool CheckClear()
         {
-            return Goals.Cast<Goal>().All(goal => goal.HasBox);
+            return _goals.Cast<Goal>().All(goal => goal.HasBox);
+        }
+
+        public static Level Create(Action<LevelData> configure)
+        {
+            LevelData data = new();
+
+            configure(data);
+
+            if (data.Box == null || data.Goal == null)
+            {
+                throw new ArgumentNullException("Box나 Goal은 필수적으로 설정되어야 합니다.");
+            }
+            if (data.Box.Count < data.Goal.Count)
+            {
+                throw new ArgumentException("Box는 Goal의 개수보다 같거나 많아야 합니다.");
+            }
+            if (data.MapSize == Size.Zero)
+            {
+                throw new ArgumentException("MapSize는 (0, 0)일 수 없습니다.");   
+            }
+
+            data.Wall = data.Wall ?? new();
+
+            return new Level(data);
         }
 
         public static Level CreateFirst()
         {
-            Level level = new Level()
+            return Create(data =>
             {
-                Player = GameObjectFactory.Create(GameObjectType.Player, Position.At(5, 10)),
-                Boxes = new List<GameObject>()
-                {
-                    GameObjectFactory.Create(GameObjectType.Box, Position.At(6, 6)),
-                    GameObjectFactory.Create(GameObjectType.Box, Position.At(8, 7)),
-                },
-                Walls = new List<GameObject>()
-                {
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(3, 3)),
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(4, 3)),
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(5, 3)),
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(6, 3)),
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(7, 3)),
-                },
-                Goals = new List<GameObject>()
-                {
-                    GameObjectFactory.Create(GameObjectType.Goal, Position.At(4, 5)),
-                    GameObjectFactory.Create(GameObjectType.Goal, Position.At(7, 10)),
-                }
-            };
-            level.Map = new(new Size(10, 10), level.Obstacles);
-
-            return level;
+                data.Player = Position.At(5, 10);
+                data.Box = new(){ Position.At(6, 6), Position.At(8, 7) };
+                data.Wall = new() { Position.At(3, 3), Position.At(4, 3), Position.At(5, 3), Position.At(6, 3), Position.At(7, 3) };
+                data.Goal = new() { Position.At(4, 5), Position.At(7, 10) };
+                data.MapSize = new(10, 10);
+            });
         }
     }
 }
