9c833f1708ed156c24fb4533dc649ad5bd10f022
smchoi <seonmun.choi@gmail.com>
2026-01-15 09:48:41 +0900

    refactor: 첫 번째 레벨과 관련된 부분을 숨김

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -7,30 +7,7 @@ namespace Sokoban
 
         static void Main(string[] args)
         {
-            Level firstLevel = new Level()
-            {
-                Player = GameObjectFactory.Create(GameObjectType.Player, Position.At(5, 10)),
-                Boxes = new List<GameObject>()
-                {
-                    GameObjectFactory.Create(GameObjectType.Box, Position.At(6, 6)),
-                    GameObjectFactory.Create(GameObjectType.Box, Position.At(8, 7)),
-                },
-                Walls = new List<GameObject>()
-                {
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(3, 3)),
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(4, 3)),
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(5, 3)),
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(6, 3)),
-                    GameObjectFactory.Create(GameObjectType.Wall, Position.At(7, 3)),
-                },
-                Goals = new List<GameObject>()
-                {
-                    GameObjectFactory.Create(GameObjectType.Goal, Position.At(4, 5)),
-                    GameObjectFactory.Create(GameObjectType.Goal, Position.At(7, 10)),
-                }
-            };
-            firstLevel.Map = new(minSize: Position.At(0, 0), maxSize: Position.At(10, 10), firstLevel.Obstacles);
-
+            Level firstLevel = Level.CreateFirst();
             IInputHandler inputHandler = new ConsoleInputHandler();
             IRenderer renderer = new ConsoleRenderer(firstLevel.AllObject);
             renderer.Prepare();
